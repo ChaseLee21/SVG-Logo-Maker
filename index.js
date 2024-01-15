cce84217df0b9e675477fbc8b94fb47271dab9e8
Tiny Penguin <chase.seeberger@gmail.com>
2024-01-15 14:47:14 -0800

    imported inquirer and setup starter questions

--- index.js
+++ index.js
@@ -4,20 +4,53 @@ const fs = require("fs");
 
 const inquirer = require("inquirer");
 
-let circle = new Circle("red");
+inquirer.prompt([
+    {
+        type: "list",
+        name: "shape",
+        message: "What shape would you like to draw?",
+        choices: ["circle", "square", "triangle"]
+    },
+    {
+        type: "input",
+        name: "color",
+        message: "What color would you like to use?"
+    }
+]).then(function (answers) {
+    let shape;
+    switch (answers.shape) {
+        case "circle":
+            shape = new Circle(answers.color);
+            break;
+        case "square":
+            shape = new Square(answers.color);
+            break;
+        case "triangle":
+            shape = new Triangle(answers.color);
+            break;
+    }
+    fs.writeFile("examples/" + answers.shape + ".svg", renderSvg(shape), function (err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("The file was saved!");
+        }
+    });
+});
 
-function renderSvg() {
+function renderSvg(shape) {
     return `<svg width="100" height="100">
-        ${circle.render()}
+    ${shape.render()}
     </svg>`;
 }
 
-fs.writeFile("examples/circle.svg", renderSvg(), function (err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("The file was saved!");
-    }
-});
+// let circle = new Circle("orange");
+// fs.writeFile("examples/circle.svg", renderSvg(circle), function (err) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("The file was saved!");
+//     }
+// });
 
 
